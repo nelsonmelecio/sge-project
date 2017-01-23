dd0daec55908ef213d59b67d9bc95741cc39330a
Nelson Melecio <ntm100986@gmail.com>
2017-01-23 20:33:29 +0800

    Installed SB Admin v2 for Laravel;

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-const elixir = require('laravel-elixir');
-
-require('laravel-elixir-vue-2');
+var elixir = require('laravel-elixir');
 
 /*
  |--------------------------------------------------------------------------
@@ -8,12 +6,29 @@ require('laravel-elixir-vue-2');
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
+ | for your Laravel application. By default, we are compiling the Less
+ | file for our application, as well as publishing vendor resources.
  |
  */
 
-elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+elixir(function(mix) {
+    mix.less('app.less');
+    mix.copy('bower_components/bootstrap/dist/fonts', 'public/assets/fonts');
+   	mix.copy('bower_components/font-awesome/fonts', 'public/assets/fonts');
+   	mix.styles([
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'bower_components/fontawesome/css/font-awesome.css',
+        'resources/css/sb-admin-2.css',
+        'resources/css/timeline.css'
+    ], 'public/assets/stylesheets/styles.css', './');
+    mix.scripts([
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bower_components/Chart.js/Chart.js',
+        'bower_components/metisMenu/dist/metisMenu.js',
+        'resources/js/sb-admin-2.js',
+        'resources/js/frontend.js'
+    ], 'public/assets/scripts/frontend.js', './');
 });
+
+
